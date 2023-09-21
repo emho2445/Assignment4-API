fada0573b18b8cf2c679bbea9d0a2418efdddfc9
Emma Hopson <emmaehopson@icloud.com>
2023-09-20 20:22:51 -0600

    Initial Commit

---
+++ Assignment4 API/Assignment4_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment4_APIApp.swift
+//  Assignment4 API
+//
+//  Created by Emma  Hopson on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment4_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
